6a7c518c2a6e78f89cc9f177841d97c9c84acd0c
Hihihi1215 <90097620+Hihihi1215@users.noreply.github.com>
2022-03-24 16:51:33 +0800

    Fade in hero words Giving bread background set and done

--- src/webPages/Home.js
+++ src/webPages/Home.js
@@ -1,23 +1,31 @@
 import React from 'react'
-import { Button } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import '../css/Home.css'
 
 
 function Home() {
   return (
-    <div>
-        <Link to='/SignIn'>
-          <Button variant="outline-primary">To Sign In</Button>
-        </Link>
-        <Link to='/SelectOrg'>
-          <Button variant="outline-primary">To Select Org</Button>
-        </Link>
-        <Link to="/RegisterAdmin">
-          <Button variant="outline-primary">To Sign Up</Button>
-        </Link>
-        <Link to="/ManageOrg">
-          <Button variant="outline-primary">To Manage Org</Button>
-        </Link>
+    <div className='home-page__wrapper'>
+        <div className='cool-hero__wrapper'>
+          <p className='cool-hero__quote'>Helping one person<br></br>
+            Might not change the world<br></br>
+            But it could<br></br>
+            Change the world for the person</p>
+        </div>
+        <section className='about-us__section'>
+          <div className='about-us__wrapper'>
+            <div className='about-us__header'>
+              <h3>About Us</h3>
+              <img src='https://fontmeme.com/permalink/220324/c167349daae29d6cb386240a08c33422.png'></img>
+            </div>
+            <div className='about-us__body'>
+              <p>We are a team of web developers inspired
+                  by the friendly neighbourhood Spider-Man.
+                  We hope to help more people in need by delivering a website to be the medium
+                  that connects non-profit organisations
+                  with the people in need</p>
+            </div>
+          </div>
+        </section>
     </div>
   )
 }
